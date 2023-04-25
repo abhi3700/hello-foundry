463c275bae2d6957a9f40eb1a1d544ed63e3ae97
abhi3700 <dalvath3700@gmail.com>
2023-04-25 22:03:20 +0530

    learned test, build, solidity compiler, optimizer

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testGet() public {
+        counter.get();
+        assertEq(counter.get(), 0);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testInc() public {
+        counter.inc();
+        assertEq(counter.get(), 1);
+    }
+
+    function testFailDec() public {
+        counter.dec();
+    }
+
+    function testDecUnderflow() public {
+        vm.expectRevert(stdError.arithmeticError);
+        counter.dec();
+    }
+
+    function testDec() public {
+        counter.inc();
+        counter.dec();
+        assertEq(counter.get(), 0);
     }
 }
